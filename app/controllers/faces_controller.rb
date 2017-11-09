e22109ac68edf7986bab2ab1df4aecc4dae8ad05
Tom <tlinford64@gmail.com>
2017-11-09 18:05:08 +0000

    Added CRUD methods

--- app/controllers/faces_controller.rb
+++ app/controllers/faces_controller.rb
@@ -1,7 +1,27 @@
 class FacesController < ApplicationController
 
+  # call the get_face method at the beginning of these actions
+  before_action :get_face, only: [:edit, :update, :show, :destroy]
+
+  def index
+    @faces = Face.all
+  end
+
   def new
     @face = Face.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @face.update(face_params)
+      flash[:notice] = "Staff member was successfully updated."
+      redirect_to face_path(@face)
+    else
+      # reload the /faces/edit page
+      render 'edit'
+    end
   end
 
   def create
@@ -17,10 +37,19 @@ class FacesController < ApplicationController
   end
 
   def show
-    @face = Face.find(params[:id])
+  end
+
+  def destroy
+    @face.destroy
+    flash[:notice] = "Staff member was successfully deleted."
+    redirect_to faces_path
   end
 
   private
+    def get_face
+      @face = Face.find(params[:id])
+    end
+
     def face_params
       params.require(:face).permit(:name, :room, :modules, :email, :photo)
     end
