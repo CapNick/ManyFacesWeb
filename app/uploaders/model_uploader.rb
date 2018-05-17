20136d0c6ddf0b227e445efc65e9f421ee1b0f36
Tom <tlinford64@gmail.com>
2018-05-17 15:42:20 +0100

    Added .fbx to list of whitelisted extensions

--- app/uploaders/model_uploader.rb
+++ app/uploaders/model_uploader.rb
@@ -36,7 +36,7 @@ class ModelUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_whitelist
-    %w(obj)
+    %w(obj fbx)
   end
 
   # Override the filename of the uploaded files:
